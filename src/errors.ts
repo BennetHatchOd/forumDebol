9b228fe0fcbced3595b4ef539c9cbe1bfa7c330f
Vladimir NG <potd3@ukr.net>
2024-08-25 16:32:32 +0300

    add error file

---
+++ src/errors.ts
@@ -0,0 +1,33 @@
+// need correct error's descriptions 
+
+export const foundError ={
+    title:{
+        message: "Title length isn't allowed",
+        field: "title"
+    },
+    shortDescription:{
+        message: "Author length isn't allowed",
+        field: "shortDescription"
+    },
+    content: {
+        message: `Resolutions must be from `,
+        field:  "content"
+    },
+    blogId: {
+        message: "canBeDownloaded isn't boolean",
+        field:  "blogId"
+    },
+    name: {
+        message: "AgeRestriction must be from 1 to 18 or nulld",
+        field:  "name"
+    },
+    websiteUrl: {
+        message: "AgeRestriction must be from 1 to 18 or nulld",
+        field:  "websiteUrl"
+    },
+    description: {
+        message: "Not correct date of publication",
+        field:  "description"
+    }
+};
+
