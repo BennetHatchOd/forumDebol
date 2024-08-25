605eb1edd790242871a151bed7c0c5bce1d1a3b4
Vladimir NG <potd3@ukr.net>
2024-08-25 14:13:20 +0300

    add types in types.ts

---
+++ src/types.ts
@@ -0,0 +1,39 @@
+
+
+type Error = {
+    message: string,
+    field: string
+}
+
+export type Errors = {
+    errorsMessages: Array<Error>
+}
+
+export type Video = {
+    id: number,
+    title:	string,
+    author:	string,
+    availableResolutions: Array<string>, 
+    canBeDownloaded:	boolean,
+    minAgeRestriction: null | number,
+    publicationDate: string
+    createdAt:	string
+}
+
+export type CreateVideo = {
+    title:	string,
+    author:	string,
+    availableResolutions: Array<string>, 
+}
+
+
+export type CorrectVideo = {
+    title:	string,
+    author:	string,
+    availableResolutions: Array<string>, 
+    canBeDownloaded:	boolean,
+    minAgeRestriction: null | number,
+    publicationDate: string
+}
+
+
