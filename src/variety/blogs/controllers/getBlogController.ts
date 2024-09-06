53d3baa7678fa4de3303eee1e68879134a7d0549
Vladimir NG <potd3@ukr.net>
2024-09-07 02:32:36 +0300

    fix bag

--- src/variety/blogs/controllers/getBlogController.ts
+++ src/variety/blogs/controllers/getBlogController.ts
@@ -1,9 +1,9 @@
-import { Response } from "express";
+import { Response, Request } from "express";
 import {HTTP_STATUSES} from '../../../setting';
 import {BlogViewModel} from '../../../types';
 import { blogRepository } from "../blogRepository";
 
-export const getBlogController = async (res: Response<BlogViewModel []>) =>{
+export const getBlogController = async (req: Request, res: Response<BlogViewModel []>) =>{
     const blogs: BlogViewModel[] = await blogRepository.view();
     res.status(HTTP_STATUSES.OK_200).json(blogs);
 
--- src/variety/posts/controllers/getPostController.ts
+++ src/variety/posts/controllers/getPostController.ts
@@ -1,9 +1,9 @@
-import {Response } from "express";
+import {Response, Request } from "express";
 import {HTTP_STATUSES} from '../../../setting';
 import { postRepository } from "../postRepository";
 import {PostViewModel} from '../../../types';
 
-export const getPostController = async (res: Response<PostViewModel[]>) =>{
+export const getPostController = async (req: Request, res: Response<PostViewModel[]>) =>{
     const posts = await postRepository.view();
     res.status(HTTP_STATUSES.OK_200).json(posts);
 
